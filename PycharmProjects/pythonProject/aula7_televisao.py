0b37db3aa735e68164135b4d3ce0ee149dfdd3e2
Alexandre Stumpf <als.stumpf@gmail.com>
2022-04-03 09:58:57 -0300

    construindo métodos, funções e classes em Python

---
+++ PycharmProjects/pythonProject/aula7_televisao.py
@@ -0,0 +1,45 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 5
+
+    def power(self):
+        if self.ligada == True:
+            self.ligada = False
+            print('Televisão desligada')
+        else:
+            self.ligada = True
+            print('Televisão ligada')
+
+    def aumenta_canal(self):
+        if self.ligada:
+            self.canal += 1
+
+    def diminui_canal(self):
+        if self.ligada:
+            self.canal -= 1
+
+televisao = Televisao()
+# print(televisao.ligada)
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+# print(televisao.ligada)
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+# print(televisao.ligada)
+print('Televisão está ligada: {}'.format(televisao.ligada))
+
+print('Canal: {}'.format(televisao.canal))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.diminui_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.power()
+print('Canal: {}'.format(televisao.canal))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.diminui_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.power()
